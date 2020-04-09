dd6d3a1a7042adc3bccfa477b0546c8b03895cfd
Javier Ramirez <44209276+javier-cr@users.noreply.github.com>
2020-04-08 18:52:08 -0700

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -1,48 +1,50 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140910195541) do
+ActiveRecord::Schema.define(version: 2014_09_10_195541) do
 
-  create_table "cores", :force => true do |t|
-    t.string   "key"
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cores", force: :cascade do |t|
+    t.string "key"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "sections", :force => true do |t|
-    t.string   "name"
-    t.string   "fullname"
-    t.string   "instructors"
-    t.integer  "seats"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.string   "days"
-    t.integer  "time_start"
-    t.integer  "time_end"
-    t.text     "description"
-    t.integer  "units"
-    t.string   "location"
-    t.string   "core"
-    t.integer  "term_id"
+  create_table "sections", force: :cascade do |t|
+    t.string "name"
+    t.string "fullname"
+    t.string "instructors"
+    t.integer "seats"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "days"
+    t.integer "time_start"
+    t.integer "time_end"
+    t.text "description"
+    t.integer "units"
+    t.string "location"
+    t.string "core"
+    t.integer "term_id"
   end
 
-  create_table "terms", :force => true do |t|
-    t.string   "name"
-    t.integer  "number"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.boolean  "default"
+  create_table "terms", force: :cascade do |t|
+    t.string "name"
+    t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "default"
   end
 
 end
